e724ece335aff8f02df286e68b854e14894a61e0
Dmytro Deker <prostodeker@gmail.com>
2025-02-05 00:27:29 +0200

    Final

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,12 @@
-function getElementWidth(content, padding, border) {
-    const contentNumber = Number.parseFloat(content);
-    const paddingNumber = Number.parseFloat(padding);
-    const borderNumber = Number.parseFloat(border);
-    const borderBox = contentNumber + paddingNumber * 2 + borderNumber * 2;
-    return borderBox;
+function checkForSpam(message) {
+  const lowerCaseMessage = message.toLowerCase();
+  return lowerCaseMessage.includes('spam') || lowerCaseMessage.includes('sale');
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); 
-console.log(getElementWidth("60px", "12px", "8.5px")); 
-console.log(getElementWidth("200px", "0px", "0px")); 
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
